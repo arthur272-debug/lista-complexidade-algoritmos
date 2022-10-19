b2928f86d9584a9ed83affe3963a88f2adc12526
Juliana Marques <115050272+juliana-marques@users.noreply.github.com>
2022-10-19 18:27:18 -0300

    Update linguagem6_exercicio1.cpp

--- exercicio_1/linguagem6_exercicio1.cpp
+++ exercicio_1/linguagem6_exercicio1.cpp
@@ -1,11 +1,12 @@
 int soma_valores(int x){
 
-int soma=0;
+int soma=0; // O(1)
 
-for(int i=0;i<=x;++i)
-    soma+=i;
+for(int i=0;i<=x;++i) // O(n)
+    soma+=i; // O(1)
 
 return soma;
 }
 
 // complexidade: O(n)
+// O(1) + O(n) + O(1) = O(n)
